e0e5d722ba45a0c275707a309512fb20d7f1080f
Max Patiiuk <max@patii.uk>
2025-06-16 11:31:28 -0700

    docs: clarify success condition

--- entry.js
+++ entry.js
@@ -5,5 +5,5 @@ const [{ utils }, { common }] = await Promise.all([
 console.log('entry', { common, utils });
 
 if (common === undefined || utils === undefined) {
-  alert('common.js or utils.js module did not load correctly.');
-}
+  alert('BUG: top-level-await module failed to load');
+} else alert('no bug: top-level-await module loaded successfully');
